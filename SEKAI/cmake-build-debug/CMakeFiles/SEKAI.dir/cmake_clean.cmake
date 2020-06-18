10e5468e81c1ce1474a364a9fada9cd567617a26
Gabriel Boff <gabrielchagasboff@gmail.com>
2020-06-18 20:44:15 -0300

    setup clion

---
+++ SEKAI/cmake-build-debug/CMakeFiles/SEKAI.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SEKAI.dir/main.cpp.o"
+  "SEKAI"
+  "SEKAI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SEKAI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
